a1fd70e99c19c8ac1ca595d8333ffaae8cebce3d
Masum Islam <masum@fasolutions.com>
2019-04-14 20:02:43 +0300

    First commit

---
+++ src/main/java/com/example/JsSqlApp/ContactController.java
@@ -0,0 +1,64 @@
+package com.example.JsSqlApp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/contacts"})
+public class ContactController {
+
+    private ContactRepository repository;
+
+    ContactController(ContactRepository contactRepository) {
+        this.repository = contactRepository;
+    }
+
+    // CRUD methods here
+
+    @GetMapping
+    public List findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<Contact> findById(@PathVariable long id){
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping
+    public Contact create(@RequestBody Contact contact){
+        return repository.save(contact);
+    }
+
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Contact> update(@PathVariable("id") long id,
+                                          @RequestBody Contact contact){
+
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+
+
+}
